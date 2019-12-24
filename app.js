30344dcabb6b2d42874ef216699c49ff5fa6e5e7
Marcus Price <marcusprice@Marcuss-iMac.local>
2019-12-24 08:50:27 -0800

    added sessions and started on login feature

--- app.js
+++ app.js
@@ -2,13 +2,48 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 const session = require('express-session')
+const bodyParser = require('body-parser')
+const credentials = require(__dirname + '/config/credentials')
+const user = require(__dirname + '/controllers/user')
+
+//app uses sessions to manage logged in state & other security measures
+app.use(session({
+  secret: credentials.sessions.secretKey,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false
+  }
+}))
 
 //sets static files to to the client build directory
 app.use(express.static(__dirname + '/client/build'))
 
 //send client app
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile('index.html')
+})
+
+//login endpoint
+app.post('/login', (req, res) => {
+
+})
+
+//check if user is logged in
+app.get('/loginCheck', (req, res) => {
+  //login status & output variable
+  const loginStatus = req.session.loggedIn
+  let output
+
+  if(!loginStatus) {
+    //if the login status is either undefined or false return false object to client
+    output = {loggedIn: false}
+  } else {
+    //the user is logged in, send true object to client
+    output = {loggedIn: true}
+  }
+
+  res.json(loginStatus)
 })
 
 app.listen(port, () => {
---
+++ controllers/user.js
@@ -0,0 +1,6 @@
+//TODO: write doc header
+const User = require('../models/db').User
+
+const checkPassword = (identifier, enteredPassword) => {
+
+}
