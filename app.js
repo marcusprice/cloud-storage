97aa64d4eeeec7c16f422d2a3f6caee285fb19bf
Marcus Price <marcusprice88@gmail.com>
2019-12-24 07:57:13 -0800

    init commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const session = require('express-session')
 
 //sets static files to to the client build directory
 app.use(express.static(__dirname + '/client/build'))
--- bin/syncDB.js
+++ bin/syncDB.js
@@ -5,11 +5,31 @@
  */
 
 const sequelize = require('../models/db').sequelize
+const User = require('../models/db').User
 
 //sync db
-sequelize.sync()
+sequelize.sync({ force: true })
   .then(() => {
-    console.log('database synced')
+    //create an example user for testing
+    return User.create({
+      firstName: 'John',
+      email: 'john@example.com',
+      password: 'password',
+      storageLimit: 2000000000
+    })
+  })
+  .then(() => {
+    //create another example user for testing
+    return User.create({
+      firstName: 'Mike',
+      email: 'mike@example.com',
+      password: 'password',
+      storageLimit: 2000000000
+    })
+  })
+  .then(() => {
+    //end the program
+    process.exit()
   })
   .catch((err) => {
     console.error(err)
